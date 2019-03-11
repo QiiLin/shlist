d0559ab388436956663fcd66ab5e936cff0f472e
Anton Kaminsky <antonwizkam@gmail.com>
2019-03-11 01:16:31 -0400

    stub out components for list view

--- src/main.js
+++ src/main.js
@@ -1,9 +1,24 @@
 import Vue from 'vue'
 import './plugins/vuetify'
 import App from './App.vue'
+import ListSelector from './components/ListSelector.vue'
+import ShoppingList from './components/ShoppingList.vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+const routes = [
+  { path: '/', component: ListSelector },
+  { path: '/list:key', component: ShoppingList },
+]
+
+const router = new VueRouter({
+  routes
+})
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
